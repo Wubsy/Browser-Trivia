dbfc315bd444376be79954f162f995f639b69543
Wubsy <willbusby39@gmail.com>
2022-01-29 10:16:38 -0600

    Initial commit

---
+++ api-control.js
@@ -0,0 +1,20 @@
+//Request API key https://opentdb.com/api_token.php?command=request
+
+
+
+
+
+//Request questions and answers https://opentdb.com/api.php?amount=10&token=YOURTOKENHERE
+
+
+
+/*
+
+
+
+
+
+
+
+
+ */
