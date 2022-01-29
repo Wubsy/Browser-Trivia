f2172bf87a0dc242d7e25a1c0986cd601d3da51b
Wubsy <willbusby39@gmail.com>
2022-01-29 13:52:18 -0600

    Added event listeners and cleaned up

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+let questionData = {
+    correct_answer: "",
+    incorrect_answers: [],
+    question: "",
+}
+
+let multipleChoiceButtons = Array.from(document.getElementsByClassName('multiple'))
+let trueFalseButtons =  Array.from(document.getElementsByClassName('tf'))
 window.onload = async () => {
-    await getAPIData(localStorage.getItem('triviaKey'))
+    questionData = await getAPIData(localStorage.getItem('triviaKey'))
+    document.getElementById('question-box').innerHTML = questionData[0].question
+
+    console.log(questionData)
+    let answers = []
+    answers.push(questionData[0].correct_answer)
+
+    for (let i = 0; i < questionData[0].incorrect_answers.length; i++) {
+        answers.push(questionData[0].incorrect_answers[i])
+    }
+
+    if (questionData[0].type === "multiple") {
+        hideElementByClassName('tf')
+        document.getElementById('a').innerHTML = answers[0]
+        document.getElementById('b').innerHTML = answers[1]
+        document.getElementById('c').innerHTML = answers[2]
+        document.getElementById('d').innerHTML = answers[3]
+    } else {
+        hideElementByClassName('multiple')
+    }
+    addListeners()
+}
+
+function addListeners() {
+    Array.from(document.getElementsByClassName('multiple')).forEach(function (element) {
+        element.addEventListener('click', function () {
+            checkAnswer(element)
+        })
+    })
+}
+
+function hideElement(element) {
+    element.setAttribute("hidden", "true")
+}
+
+function checkAnswer(element) {
+    if (element.innerHTML === questionData[0].correct_answer) {
+        console.log("Correct")
+    } else {
+        console.log("Nope")
+    }
+}
+
+function hideElementByClassName(className) {
+    Array.from(document.getElementsByClassName(className)).forEach(function(element) {
+        hideElement(element)
+    })
 }
